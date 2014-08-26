36dcfd1b247f1fd8d9ea1a4a68b64972eeb4add3
Jan Raasch <jan@janraasch.com>
2014-08-26 14:32:52 +0200

    Add spec_helper to mock config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,11 @@
+require 'pry'
 require File.join(File.dirname(__FILE__), '/../lib/file_convert')
+
+def configure_with_mock
+  FileConvert.configure do |config|
+    config['google_service_account'] = {
+      'email' => '<strange-hash>@developer.gserviceaccount.com',
+      'pkcs12_file_path' => 'config/file_convert_key.p12'
+    }
+  end
+end
