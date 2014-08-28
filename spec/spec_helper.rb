4eff1263d09cb32c8879885b6ec4f9410a7ec68f
Jan Raasch <jan@janraasch.com>
2014-08-28 18:11:40 +0200

    Remove non-sense

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,12 +14,3 @@ RSpec.configure do |config|
     mock_config.syntax = [:expect]
   end
 end
-
-def configure_with_mock
-  FileConvert.configure do |config|
-    config['google_service_account'] = {
-      'email' => '<strange-hash>@developer.gserviceaccount.com',
-      'pkcs12_file_path' => 'config/file_convert_key.p12'
-    }
-  end
-end
