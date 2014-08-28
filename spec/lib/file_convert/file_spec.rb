120f1a012e43b521ffdbd937a1b6d330f32ce992
Jan Raasch <jan@janraasch.com>
2014-08-28 17:38:58 +0200

    Fix file_spec

--- spec/lib/file_convert/file_spec.rb
+++ spec/lib/file_convert/file_spec.rb
@@ -1,35 +1,29 @@
 require 'spec_helper'
 
 describe FileConvert::Conversion do
-  before(:all) { FileConvert::Configure::init_config! }
-
-  let(:original_file) { double('file', data: 'tanzbar') }
-  subject { @file }
-
-  before :each do
-    @file = FileConvert::File.new(original_file)
-  end
+  let(:original_file) { double 'file', data: :data }
+  let(:file) { FileConvert::File.new original_file }
+  subject { file }
 
   describe '#initialize' do
-    it { is_expected.to be_a(FileConvert::File) }
+    it { is_expected.to be_a FileConvert::File }
   end
 
   describe '#data' do
-    subject { @file.data }
-    it { is_expected.to eq('tanzbar') }
+    subject { file.data }
+    it { is_expected.to eq :data }
   end
 
   describe '#conversions' do
-    subject { @file.conversions }
+    subject { file.conversions }
 
     context 'after initialization' do
-      it { is_expected.to eq({}) }
+      it { is_expected.to be_empty }
     end
 
     context 'after adding a conversion' do
-      before { @file.add_conversion('foo', 'bar') }
-      it { is_expected.to eq({ 'foo' => 'bar' }) }
+      before { file.add_conversion(:foo, :bar) }
+      it { is_expected.to eq foo: :bar }
     end
   end
-
 end
