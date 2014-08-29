3bf599aef7aa302edaa116a2bcccebe9a34b18fd
Jan Raasch <jan@janraasch.com>
2014-08-29 14:09:13 +0200

    Add integration test

---
+++ spec/integration/file_convert_integration.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+
+describe 'Google API' do
+  before do
+    FileConvert::Configure.init_config!
+  end
+
+  # Remember that this `subject` triggers an API call
+  subject do
+    FileConvert.convert(file_path, source_mime_type, target_mime_type)
+  end
+
+  context 'can convert txt to pdf and' do
+    let(:file_path) do
+      File.join File.dirname(__FILE__), '..', 'fixtures', 'test.txt'
+    end
+    let(:converted_file_path) do
+      File.join File.dirname(__FILE__), '..', 'fixtures', 'test.txt.pdf'
+    end
+    let(:source_mime_type) { 'text/plain' }
+    let(:target_mime_type) { 'application/pdf' }
+
+    it 'integrates perfectly' do
+      skip
+      expect(subject).to be_a FileConvert::Conversion
+
+      expect(subject.body).to be_a String
+      expect(subject.body).not_to be_empty
+
+      expect(subject.file).to be_a Google::APIClient::Result
+
+      tempfile = Tempfile.new file_path
+      subject.save tempfile.path
+      expect(
+        FileUtils.compare_file converted_file_path, tempfile.path
+      ).to be true
+      tempfile.close
+      tempfile.unlink
+    end
+  end
+
+  context 'can convert doc to docx and' do
+    let(:file_path) do
+      File.join File.dirname(__FILE__), '..', 'fixtures', 'test.doc'
+    end
+    let(:converted_file_path) do
+      File.join File.dirname(__FILE__), '..', 'fixtures', 'test.doc.docx'
+    end
+    let(:source_mime_type) { 'application/msword' }
+    let(:target_mime_type) do
+      'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+    end
+
+    it 'integrates perfectly' do
+      expect(subject).to be_a FileConvert::Conversion
+
+      expect(subject.body).to be_a String
+      expect(subject.body).not_to be_empty
+
+      expect(subject.file).to be_a Google::APIClient::Result
+      # for some reason two converted docx files are not the same
+      # when comparing with `FileUtils.compare_file`
+    end
+  end
+end
