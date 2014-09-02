f12fefb23644a7f59b17ff17ffdcbaeab29b4cca
Christoph Hugo <christoph.hugo@tolingo.com>
2014-09-02 16:08:38 +0200

    Removes YAML config and updates specs

--- lib/file_convert/configure.rb
+++ lib/file_convert/configure.rb
@@ -1,28 +1,11 @@
 module FileConvert
   module Configure
-    require 'yaml'
-
-    DEFAULT_CONFIG_PATH = 'config/file_convert.yml'
-
-    def self.config_yaml_present?
-      ::File.exist?(DEFAULT_CONFIG_PATH)
-    end
-
-    def self.config_from_yaml
-      ::File.open(DEFAULT_CONFIG_PATH) do |file|
-        YAML.load(file)['file_convert']
-      end
-    end
-
     def self.config
       @@config
     end
 
-    ###
-    # Loads config/file_convert.yml['file_convert'] into Config
-    # Or defaults to empty Hash if config file is not present
     def self.init_config!
-      @@config = config_yaml_present? ? config_from_yaml : {}
+      @@config ||= {}
     end
 
     def configure
