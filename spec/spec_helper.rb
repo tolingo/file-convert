6ca84baa7c0aa6ea88bbc1946aa70bb52e7caf00
Jan Raasch <jan@janraasch.com>
2014-09-08 17:24:51 +0200

    Update spec_helper to rspec 3.1.0 defaults

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,9 +11,17 @@ require 'pry'
 require 'file_convert'
 
 RSpec.configure do |config|
-  config.mock_with :rspec do |mock_config|
-    mock_config.verify_doubled_constant_names = true
-    mock_config.verify_partial_doubles = true
-    mock_config.syntax = [:expect]
+  config.expect_with :rspec do |expectations|
+    # This option will default to `true` in RSpec 4.
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.verify_doubled_constant_names = true
+    mocks.verify_partial_doubles = true
+    mocks.syntax = [:expect]
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object. This is generally recommended, and will default to
+    # `true` in RSpec 4.
+    mocks.verify_partial_doubles = true
   end
 end
